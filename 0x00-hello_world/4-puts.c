394ff5f3c909c96c2dbaf1c0a0ca75cf879b9744
Bam9719 <bamlak0119@gmail.com>
2022-03-10 14:40:34 -0800

    my commit massage

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdo.h>
+/**
+ *main - entry point
+ *
+ *return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzel");
+return (0);
+
+}
